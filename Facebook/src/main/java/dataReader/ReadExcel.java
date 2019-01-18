10f6abbecde2952d92431c3b4f0a7c0a78c21433
username <mmiahny@gmail.com>
2019-01-18 01:37:08 -0500

    Update excel data reader

---
+++ Facebook/src/main/java/dataReader/ReadExcel.java
@@ -0,0 +1,22 @@
+package dataReader;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ReadExcel {
+    public static void main(String[]args) throws Exception{
+        File src = new File("C:\\Users\\Moniruzzaman Miah\\Desktop\\java1\\webautomationteam\\Facebook\\src\\main\\java\\testData\\FBtestData.xlsx");
+        FileInputStream fis = new FileInputStream(src);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet1 = wb.getSheetAt(0);
+        String data0 = sheet1.getRow(0).getCell(0).getStringCellValue();
+        System.out.println("Data From Excel is: "+data0);
+        String data1 = sheet1.getRow(0).getCell(1).getStringCellValue();
+        System.out.println("Data From Excel is: "+data1);
+
+
+    }
+}
